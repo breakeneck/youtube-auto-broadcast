4f94def30bd671615d108fddad64d02d052dcdc3
Yuri <breakeneck@gmail.com>
2023-04-10 15:51:12 +0300

    Implenented tg bot updates

--- src/Telegram.php
+++ src/Telegram.php
@@ -30,8 +30,52 @@ class Telegram
             ])
             ->post('https://api.whatsmate.net/v3/telegram/group/text/message/{id}', ['{id}' => $INSTANCE_ID]);
     }
+
+
+
+{
+    "update_id": 827535379,
+    "message": {
+        "message_id": 21716,
+        "from": {
+            "id": 390016459,
+            "is_bot": false,
+            "first_name": "Yuri",
+            "last_name": "D",
+            "username": "breakeneck",
+            "language_code": "en"
+        },
+        "chat": {
+            "id": -1001197280278,
+            "title": "\u0425\u0430\u0440\u0435 \u041a\u0440\u0456\u0448\u043d\u0430 (\u041b\u0443\u0446\u044c\u043a\u0430 \u044f\u0442\u0440\u0430)",
+            "type": "supergroup"
+        },
+        "date": 1681130221,
+        "new_chat_participant": {
+            "id": 211246197,
+            "is_bot": true,
+            "first_name": "Telegram Bot Raw",
+            "username": "RawDataBot"
+        },
+        "new_chat_member": {
+            "id": 211246197,
+            "is_bot": true,
+            "first_name": "Telegram Bot Raw",
+            "username": "RawDataBot"
+        },
+        "new_chat_members": [
+            {
+                "id": 211246197,
+                "is_bot": true,
+                "first_name": "Telegram Bot Raw",
+                "username": "RawDataBot"
+            }
+        ]
+    }
+}
+
 */
-    public function send($chatId, $text)
+    public function message($chatId, $text)
     {
         return (new Request())
             ->setData([
@@ -39,5 +83,14 @@ class Telegram
                 'text' => $text
             ])
             ->get('https://api.telegram.org/bot{token}/sendMessage', ['{token}' => $this->token]);
+    }
+
+    /**
+     * Retrieving all bot updates, including chat_id and groups added
+     * @return \Breakeneck\Http\Response
+     */
+    public function updates()
+    {
+        return (new Request())->post('https://api.telegram.org/bot{token}/getUpdates', ['{token}' => $this->token]);
     }
 }
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$response = (new \App\Telegram($_ENV['TG_API_TOKEN']))->updates();
+print_r($response->content);
