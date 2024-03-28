7e9c206b389854f25f92ccda7c0d3c29877637f8
Yuri Datsenko <breakeneck@gmail.com>
2024-03-28 10:00:42 +0200

    test updates

--- backend/console/test.php
+++ backend/console/test.php
@@ -9,9 +9,6 @@ $dotenv->load();
 //$response = (new App\Telegram($_ENV['TG_API_TOKEN']))->updates();
 //print_r($response->content);
 
-//RD1zZRWtq0M
-
-
 $scenario = new App\Scenario();
 //$scenario->camera->zoomIn();
 //$broadcastId = $scenario->startBroadcast('Title', 120);
