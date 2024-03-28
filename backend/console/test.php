cf4f80ced04dca04e7a0c4bf59177af04f3e7ac3
Yuri Datsenko <breakeneck@gmail.com>
2024-03-28 09:42:50 +0200

    Updated test action

--- backend/console/test.php
+++ backend/console/test.php
@@ -5,5 +5,16 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$response = (new App\Telegram($_ENV['TG_API_TOKEN']))->updates();
-print_r($response->content);
+//$response = (new App\Telegram($_ENV['TG_API_TOKEN']))->updates();
+//print_r($response->content);
+
+//RD1zZRWtq0M
+
+
+$scenario = new App\Scenario();
+//$scenario->camera->zoomIn();
+//$broadcastId = $scenario->startBroadcast('Title', 120);
+$scenario->notify('RD1zZRWtq0M');
+//$scenario->wait($length);
+//$scenario->finishBroadcast($broadcastId);
+//$scenario->camera->zoomOut();
