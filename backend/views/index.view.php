160ef9acdc2c25bb005bd9d50e19e32c232b38f4
Yuri Datsenko <breakeneck@gmail.com>
2024-07-18 15:14:54 +0300

    Implemented styles for finish broadcast

--- backend/views/index.view.php
+++ backend/views/index.view.php
@@ -10,6 +10,7 @@
             crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>ІССКОН Луцьк: пряма трансляція</title>
 </head>
 <body>
@@ -32,12 +33,13 @@
 </style>
 
 <?php if ($state->getAttr('id')):?>
-    <a href="https://www.youtube.com/watch?v=<?=$state->getAttr('id')?>">Live</a>
-    <br/>
-    <br/>
-    <br/>
+
     <form method="post" action="/stop">
-        <button>Завершити</button>
+        <a class="icon-link" href="https://www.youtube.com/watch?v=<?=$state->getAttr('id')?>">
+            <i class="bi bi-youtube"></i>
+            Youtube
+        </a>
+        <button type="submit" class="btn btn-primary">Завершити трансляцію</button>
     </form>
 <?php else: ?>
 
