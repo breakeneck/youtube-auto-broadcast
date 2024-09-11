2f90a40251ae91b12c5ecb82b05347ae723c1f89
Yuri Datsenko <breakeneck@gmail.com>
2024-09-11 17:15:54 +0300

    Fixed retrieval from google sheets

--- backend/index.php
+++ backend/index.php
@@ -24,66 +24,7 @@ app()->config(['debug' => $_ENV['APP_DEBUG']]);
 
 
 app()->get('/', function () use ($state) {
-
-// Replace with your downloaded JSON credentials file path
-//    putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/data/youtubebroadcastersheets-4974f2784de2.json');
-//    $credentialsPath = __DIR__ . '/data/youtubebroadcastersheets-4974f2784de2.json';
-
-//    $spreadsheetId = '1ai7ze0rTnOR6wAOkSl3Lc8x0v-M1cO_qJfE7Bvv3C5I'; // Replace with your spreadsheet ID
-//    $sheetId = '219376182';
-//    $sheetId = 'SB';
-
-    $client = new Google\Client();
-    $client->setApplicationName('My PHP Script');
-    $client->setScopes(['https://www.googleapis.com/auth/spreadsheets.readonly']);
-    $client->setAuthConfig($_ENV['SHEETS_CREDENTIALS']); // Load credentials
-
-    $service = new Google\Service\Sheets($client);
-//    $response = $service->spreadsheets->get($spreadsheetId); // Get spreadsheet info
-//    $sheets = [];
-//    foreach ($response->getSheets() as $sheetData) {
-//        print_r($sheetData['properties']);
-////        $sheets[] = print_r($sheetData['properties'])['id']; // Extract sheet title
-//    }
-//
-//    echo "Available sheets:\n";
-//    echo implode(", ", $sheets) . "\n";
-
-    $range = $_ENV['SHEET_ID'] . "!A:E"; // Adjust range based on your sheet data
-
-    $response = $service->spreadsheets_values->get($_ENV['SPREADSHEET_ID'], $range);
-    $rows = $response->getValues();
-
-    $dateTimeObj = new DateTime('now', new DateTimeZone('Europe/Kiev'));
-    $formatted = IntlDateFormatter::formatObject($dateTimeObj,'EEEE dd.MM.Y','uk');
-    echo "Сьогодні $formatted\n";
-//    print_r($rows);
-    $n = 0;
-    $prevDate = null;
-    foreach ($rows as $row) {
-        if ($row[0] == date('d.m.Y')) {
-            $n = 1;
-        }
-        if ($n) {
-            if ($prevDate && DateTime::createFromFormat($row[0],'d.m.Y')->format('N') < $prevDate->format('N')) {
-                break;
-            }
-            if (isset($row[2]) && $row[2]) {
-                $lastRows[] = $row;
-            }
-            if ($n > 7) {
-                break;
-            }
-            if ($row[0]) {
-                $prevDate = DateTime::createFromFormat($row[0],'d.m.Y');
-            }
-            $n++;
-        }
-    }
-
-//    print_r($lastRows);
-
-//    $lastRows = array_slice($rows, -10); // Get last 10 rows
+    $lastRows = getLastRowsFromExcelFile();
 
     echo app()->template->render('index', [
         'state' => $state,
@@ -123,3 +64,52 @@ app()->post('/stop', function () use ($state) {
 });
 
 app()->run();
+
+
+function getLastRowsFromExcelFile()
+{
+    $lastRows = [];
+    try {
+        $client = new Google\Client();
+        $client->setApplicationName('My PHP Script');
+        $client->setScopes(['https://www.googleapis.com/auth/spreadsheets.readonly']);
+        $client->setAuthConfig($_ENV['SHEETS_CREDENTIALS']); // Load credentials
+
+        $service = new Google\Service\Sheets($client);
+        $range = $_ENV['SHEET_ID'] . "!A:E"; // Adjust range based on your sheet data
+
+        $response = $service->spreadsheets_values->get($_ENV['SPREADSHEET_ID'], $range);
+        $rows = $response->getValues();
+
+        $dateTimeObj = new DateTime('now', new DateTimeZone('Europe/Kiev'));
+        $formatted = IntlDateFormatter::formatObject($dateTimeObj, 'EEEE dd.MM.Y', 'uk');
+        echo "Сьогодні $formatted\n";
+//    print_r($rows);
+        $n = 0;
+        $prevDate = null;
+        foreach ($rows as $row) {
+            if ($row[0] == date('d.m.Y')) {
+                $n = 1;
+            }
+            if ($n) {
+                if ($prevDate && DateTime::createFromFormat($row[0], 'd.m.Y')->format('N') < $prevDate->format('N')) {
+                    break;
+                }
+                if (isset($row[2]) && $row[2]) {
+                    $lastRows[] = $row;
+                }
+                if ($n > 7) {
+                    break;
+                }
+                if ($row[0]) {
+                    $prevDate = DateTime::createFromFormat($row[0], 'd.m.Y');
+                }
+                $n++;
+            }
+        }
+    }
+    catch (\Exception $exception) {
+
+    }
+    return $lastRows;
+}
