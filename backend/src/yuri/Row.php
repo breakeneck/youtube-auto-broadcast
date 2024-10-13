c2c0b6fb3fc8f6ee49f8cea7505d104fe9be0943
Yuri Datsenko <breakeneck@gmail.com>
2024-10-13 14:03:37 +0300

    Fixed app row not found

---
+++ backend/src/yuri/Row.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App;
+
+const UA_DATE_FORMAT = 'd.m.Y';
+class Row {
+    public $isManualMode;
+    public $date;
+    public $book;
+    public $verse;
+    public $username;
+//    public $title;
+
+    public function __construct($isManualMode = false, $date = null, $dayOfWeek = null, $book = null, $verse = null, $username = null)
+    {
+        $this->isManualMode = (bool)$isManualMode;
+        $this->date = $date ? \DateTime::createFromFormat(UA_DATE_FORMAT, $date) : null;
+        $this->book = $book;
+        $this->verse = $verse;
+        $this->username = $username;
+    }
+
+    public function isValid()
+    {
+        return $this->book && $this->verse && $this->username;
+    }
+
+    function isToday(): bool
+    {
+        return $this->date instanceof \DateTime && $this->date->format(UA_DATE_FORMAT) == date(UA_DATE_FORMAT);
+    }
+
+    function isWeekTransition($prevRow): bool
+    {
+        return $prevRow->date instanceof \DateTime && $this->date instanceof \DateTime
+            && $this->date->format('N') < $prevRow->date->format('N');
+    }
+
+    function dateFormatted()
+    {
+        return $this->date instanceof \DateTime ? $this->date->format(UA_DATE_FORMAT) : '';
+    }
+
+    function dayOfWeek()
+    {
+        if ($this->date instanceof \DateTime) {
+            return Utils::getLocalTimeStr($this->date->format('Y-m-d'), 'EEEEEE');
+        }
+        return '';
+    }
+}
